102423fe1e3906e1bfd98a8bf791c54bdb4b0a0e
Adam Bodie <aduo3000@yahoo.com>
2016-03-20 14:41:41 -0700

    Updated time.js with new data

--- public/js/time.js
+++ public/js/time.js
@@ -9,7 +9,7 @@ var date = [
 {id: "daysAgo7", year: 2015, month: 7, day: 8, updateId: 'updateDaysAgo7', updateYear: 2015, updateMonth: 7, updateDay: 8 },
 {id: "daysAgo8", year: 2015, month: 7, day: 18, updateId: 'updateDaysAgo8', updateYear: 2015, updateMonth: 7, updateDay: 18 },
 {id: "daysAgo9", year: 2015, month: 8, day: 7, updateId: 'updateDaysAgo9', updateYear: 2015, updateMonth: 8, updateDay: 19 },
-{id: "daysAgo10", year: 2016, month: 2, day: 13, updateId: 'updateDaysAgo10', updateYear: 2016, updateMonth: 2, updateDay: 15 }
+{id: "daysAgo10", year: 2016, month: 2, day: 13, updateId: 'updateDaysAgo10', updateYear: 2016, updateMonth: 2, updateDay: 19 }
 ]
 
 var current = new Date();
