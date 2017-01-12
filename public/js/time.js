40ce55779e8f42567908f96be6c4065c86c61ffe
Adam Bodie <aduo3000@yahoo.com>
2017-01-12 15:58:42 -0800

    Updated recent updates section, removed picture

--- public/js/time.js
+++ public/js/time.js
@@ -7,7 +7,7 @@ var date = [
 {id: "daysAgo5", year: 2015, month: 6, day: 17, updateId: 'updateDaysAgo5', updateYear: 2015, updateMonth: 10, updateDay: 11 },
 {id: "daysAgo6", year: 2015, month: 6, day: 25, updateId: 'updateDaysAgo6', updateYear: 2017, updateMonth: 0, updateDay: 8 },
 {id: "daysAgo7", year: 2015, month: 7, day: 8, updateId: 'updateDaysAgo7', updateYear: 2015, updateMonth: 7, updateDay: 8 },
-{id: "daysAgo8", year: 2015, month: 7, day: 18, updateId: 'updateDaysAgo8', updateYear: 2015, updateMonth: 7, updateDay: 18 },
+{id: "daysAgo8", year: 2015, month: 7, day: 18, updateId: 'updateDaysAgo8', updateYear: 2017, updateMonth: 0, updateDay: 12 },
 {id: "daysAgo9", year: 2015, month: 8, day: 7, updateId: 'updateDaysAgo9', updateYear: 2016, updateMonth: 7, updateDay: 28 },
 {id: "daysAgo10", year: 2016, month: 2, day: 13, updateId: 'updateDaysAgo10', updateYear: 2017, updateMonth: 0, updateDay: 11 },
 {id: "daysAgo11", year: 2016, month: 2, day: 24, updateId: 'updateDaysAgo11', updateYear: 2016, updateMonth: 2, updateDay: 24 },
