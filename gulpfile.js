1c992740292c5aac37fc0e75102dce030c580b75
Adam Bodie <aduo3000@yahoo.com>
2017-01-29 11:32:59 -0800

    Added src folder for vendor, scripts and styles, updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,37 +8,39 @@ var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
 var jsValidate = require('gulp-jsvalidate');
 var jsonlint = require("gulp-jsonlint");
- 
+
 var destJS = './public/js/';
 var destCSS = './public/css/';
+var vendor = './src/vendor/';
+var scripts = './src/scripts/';
+var styles = './src/styles';
 
-
-gulp.task('scrollMe', function() {
-  return gulp.src(['./public/scroll-up-bar/scroll-up-bar.js', './public/js/scroll.js'])
+gulp.task('createScrollBar', function() {
+  return gulp.src([vendor + 'scroll-up-bar/scroll-up-bar.js', './public/js/scroll.js'])
     .pipe(concat('./scrollbar.js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest(destJS));
 });
 
-gulp.task('tickerMe', function() {
-  return gulp.src(['./public/vticker/jquery.vticker.js', './public/js/ticker.js'])
+gulp.task('createVticker', function() {
+  return gulp.src([vendor + '/vticker/jquery.vticker.js', './public/js/ticker.js'])
     .pipe(concat('./vticker.js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest(destJS));
 });
 
-gulp.task('openDate', function() {
-  return gulp.src(['./public/js/open.js', './public/js/date.js'])
+gulp.task('createOpenOnDate', function() {
+  return gulp.src([scripts + 'open.js', scripts + 'date.js'])
     .pipe(concat('./open-on-date.js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest(destJS));
 });
 
-gulp.task('imageLightbox', function() {
-  return gulp.src(['./public/js/imagelightbox.js', './public/js/myLightbox.js'])
+gulp.task('createImageLightbox', function() {
+  return gulp.src([vendor + '/imageLightbox/imagelightbox.js', './public/js/myLightbox.js'])
     .pipe(concat('./myImageLightbox.js'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
@@ -46,14 +48,14 @@ gulp.task('imageLightbox', function() {
 });
 
 gulp.task('minify', function () {
-	return gulp.src(['./' + destJS + 'feature.js', './' + destJS + 'overlay.js', './' + destJS + 'locations.js', './' + destJS + 'howManyDaysAgo.js', './' + destJS + 'time.js', './' + destJS + 'ad.js'])
+	return gulp.src(['./' + scripts + 'feature.js', './' + scripts + 'overlay.js', './' + scripts + 'locations.js', './' + scripts + 'howManyDaysAgo.js', './' + scripts + 'updateDaysAgo.js', './' + scripts + 'ad.js'])
 	.pipe(rename({suffix: '.min'}))
 	.pipe(uglify())
 	.pipe(gulp.dest(destJS));
 });
 
-gulp.task('sassMe', function () {
-	return gulp.src('./public/css/main.scss')
+gulp.task('turnSassToCss', function () {
+	return gulp.src(styles + 'main.scss')
 	.pipe(maps.init())
 	.pipe(sass())
 	.pipe(maps.write('./'))
@@ -65,6 +67,6 @@ gulp.task('valid', function () {
     .pipe(jsValidate());
 });
 
-gulp.task('default', ['valid', 'scrollMe', 'tickerMe', 'openDate', 'imageLightbox', 'minify', 'sassMe'], function () {
+gulp.task('default', ['valid', 'createScrollBar', 'createVticker', 'createOpenOnDate', 'createImageLightbox', 'minify', 'turnSassToCss'], function () {
 	console.log("All tasks completed");
 });
--- src/scripts/date.js
+++ src/scripts/date.js
@@ -1,3 +1,3 @@
-			var date = new Date();		
+			var date = new Date();
 			var hours = open(date.getFullYear(), date.getMonth(), date.getDate(), date.getDay(), date.getHours());
 			document.getElementById("container").innerHTML = "<h3>" + hours + "</h3>";
