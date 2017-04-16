9740edb0ec503548e16dcdc836c49ee923de492a
Adam Bodie <aduo3000@yahoo.com>
2017-04-16 12:44:01 -0700

    Added SkillsController to about page

---
+++ public/js/controllers/SkillsController.js
@@ -0,0 +1,4 @@
+
+app.controller('SkillsController', ['$scope', function($scope) {
+    $scope.skills = ["HTML5", "CSS3", "JavaScript", "jQuery", "AngularJS", "React", "Ember", "NodeJS", "Express"];
+  }]);
