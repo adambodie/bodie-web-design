b2101f4bfc8bef234282433ccfc5d4ddd0344b77
adambodie <aduo3000@yahoo.com>
2017-04-16 07:56:51 -0700

    Added update list to Main Controller

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -63,6 +63,34 @@ angular.module('mainApp', [])
     }
 
     $scope.title = "Bodie Web Design";
+    $scope.updates = [
+		{
+			title: "NFL Passer Rating",
+			description: "Launched April 2, 2017"
+		},
+		{
+			title: "Florence Foundations",
+			description: "Launched February 19, 2017"
+		},
+		{
+			title: "Coen Brothers Film Review",
+			description: "Launched February 9, 2017"
+		},
+		{
+			title: "Dreidel Duel!",
+			description: "Launched January 22, 2017"
+		},
+		{
+			title: "Adam's Wonderful World of Waterfalls",
+			description: "Updated January 11, 2017, replaced with Beta Version.  See note below."
+		},
+		{
+			title: "Duck vs. Blue Hen!",
+			description: "Updated January 8, 2017 with redesign, Slick jQuery plugin"
+		}
+												
+		
+    ]
     $scope.projects = [
       {
         image: "grandcanyon",
