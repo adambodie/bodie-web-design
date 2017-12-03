7ebc16249ceaf84b9f353d40f08df130bc867d70
Adam Bodie <adam.bodie@yahoo.com>
2017-12-02 17:12:50 -0800

    added reverse filter to reverse order of projects

--- public/js/angular/main.js
+++ public/js/angular/main.js
@@ -1,1 +1,6 @@
-var app = angular.module('mainApp', ['ngResource', 'ui.bootstrap', 'ngMap']);
+var app = angular.module('mainApp', ['ngResource', 'ui.bootstrap', 'ngMap'])
+.filter("reverse", function(){
+    return function(items){
+        return items.slice().reverse(); // Create a copy of the array and reverse the order of the items
+    };
+});    
