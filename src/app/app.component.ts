eedf619d1ba0d034fa3c8c4010a67ce9cfda6656
Adam Bodie <adam.bodie@yahoo.com>
2018-06-28 19:03:42 -0700

    Cleaned merge conflicts, added Sass, basic index layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-=======
   templateUrl: './app.component.pug',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
->>>>>>> 78669965e0a833fc1617a0ee441744bbae60016e
 }
