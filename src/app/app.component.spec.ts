d8f3896908e2ad5c4bab89e08ba7b43186ee477b
Adam Bodie <adam.bodie@yahoo.com>
2018-07-01 10:54:11 -0700

    Updated App component testing suite

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,50 @@
-import { TestBed, async } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+@Component({selector: 'app-navbar', template: ''})
+class NavbarStubComponent {}
+
+@Component({selector: 'app-footer', template: ''})
+class FooterStubComponent {}
+
+@Component({selector: 'app-projects', template: ''})
+class ProjectsStubComponent {}
+
+@Component({selector: 'app-feature', template: ''})
+class FeatureStubComponent {}
+
+@Component({selector: 'app-picture-week', template: ''})
+class PictureWeekStubComponent {}
+
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture:   ComponentFixture<AppComponent>;
+  let h2:        HTMLElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarStubComponent,
+        FooterStubComponent,
+        ProjectsStubComponent,
+        FeatureStubComponent,
+        PictureWeekStubComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    h2 = fixture.nativeElement.querySelector('h2');
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should render title in a h2 tag', async(() => {
+    component.title = 'Welcome to Bodie Web Design';
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h2').textContent).toContain('Welcome to Bodie Web Design');
+    expect(compiled.querySelector('h2').textContent).toContain(component.title);
   }));
 });
