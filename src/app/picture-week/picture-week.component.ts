4e5ef55827df5be713319d5467fa8f3828b155db
Adam Bodie <adam.bodie@yahoo.com>
2018-07-07 12:48:18 -0700

    Organized styles

--- src/app/picture-week/picture-week.component.ts
+++ src/app/picture-week/picture-week.component.ts
@@ -30,4 +30,14 @@ export class PictureWeekComponent implements OnInit {
     this.location = this.pictures[this.pictures.length - 1].location;
     this.state = this.pictures[this.pictures.length - 1].state;
   }
+  updates = [
+    { title: "Bellingham Native", description: "Launched April 23, 2018, built with React Native"},   
+    { title: "Read Your Fortune Cookie", description: "Upgraded to Angular & TypeScript, January 28, 2018"},
+    { title: "Kevin's Fun Facts", description: "Launched January 1, 2018, built with Android and Kotlin"}, 
+    { title: "NFL Passer Rating", description: "Reconfigured components to allow flexibility of button colors, January 7, 2018"},      
+    { title: "Dreidel Duel", description: "Added new feature, converted syntax to ES6, January 1, 2018" },       
+    { title: "Reno Nice 'n' Cool", description: "Launched version 2.0 December 11, 2017, built with Vue.js" },      
+    { title: "Adam's Adventures", description: "Launched November 23, 2017, built with React" },
+    { title: "Adam's Wild Wild West", description: "Launched October 1, 2017, built with React"}
+  ]  
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -21,30 +21,4 @@ export class ProjectsComponent implements OnInit {
     this.projects = this.projectsService.getProjects().slice().reverse();
     this.mobileProjects = this.projectsService.getMobileProjects().slice().reverse();
   }
-  updates = [
-    {
-      title: "Read Your Fortune Cookie",
-      description: "Upgraded to Angular & TypeScript, January 28, 2018"
-    },
-    {
-      title: "NFL Passer Rating",
-      description: "Reconfigured components to allow flexibility of button colors, January 7, 2018"
-    },      
-    {
-      title: "Dreidel Duel",
-      description: "Added new feature, converted syntax to ES6, January 1, 2018"
-    },       
-    {
-      title: "Reno Nice 'n' Cool",
-      description: "Launched version 2.0 December 11, 2017, built with Vue.js"
-    },      
-    {
-      title: "Adam's Adventures",
-      description: "Launched November 23, 2017, built with React"
-    },
-    {
-      title: "Adam's Wild Wild West",
-      description: "Launched October 1, 2017, built with React"
-    }
-  ]
 }
