1867bb990201c2849259929672fa09cf24d933a0
adambodie <adam.bodie@yahoo.com>
2018-07-09 20:14:56 -0700

    Updated feature component, testing spec

--- src/app/feature/feature.component.spec.ts
+++ src/app/feature/feature.component.spec.ts
@@ -3,23 +3,42 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FeatureComponent } from './feature.component';
 
 describe('FeatureComponent', () => {
-  let component: FeatureComponent;
-  let fixture: ComponentFixture<FeatureComponent>;
+	let component: FeatureComponent;
+	let fixture: ComponentFixture<FeatureComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FeatureComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ FeatureComponent ]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FeatureComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(FeatureComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should show a title', () => {
+		const expectedTitle = 'randomTitle';
+		component.randomTitle = expectedTitle;
+		expect(component.randomTitle).toBe('randomTitle');
+	});
+	
+	
+	it('should show a link', () => {
+		const expectedLink = 'randomLink';
+		component.randomLink = expectedLink;
+		expect(component.randomLink).toBe('randomLink');
+	});
+	
+	it('should show an image', () => {
+		const expectedImage = 'randomImage';
+		component.randomImage = expectedImage;
+		expect(component.randomImage).toBe('randomImage');
+	});	
 });
--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -3,28 +3,27 @@ import {ProjectsService} from '../services/projects.service';
 import {Project} from '../../properties/project';
 
 @Component({
-  selector: 'app-feature',
-  templateUrl: './feature.component.pug',
-  styleUrls: ['./feature.component.scss'],
-  providers: [ProjectsService]
+	selector: 'app-feature',
+	templateUrl: './feature.component.pug',
+	styleUrls: ['./feature.component.scss'],
+	providers: [ProjectsService]
 })
 export class FeatureComponent implements OnInit {
-  constructor(private projectsService: ProjectsService) { }
-  projects: Project[];  
-  randomImage;
-  randomLink;
-  randomTitle;
+	constructor(private projectsService: ProjectsService) { }
+	projects: Project[];  
+	randomImage;
+	randomLink;
+	randomTitle;
 
-  ngOnInit() {
-    this.getProjects();
-  }
-  getProjects(): void {
-    this.projects = this.projectsService.getProjects().slice().reverse();
-    let random = Math.floor(Math.random() * this.projects.length);
-    this.randomImage = this.projects[random].image;
-    this.randomLink = this.projects[random].link;
-    this.randomTitle = this.projects[random].title;
-  }
-
-
+	ngOnInit() {
+		this.getProjects();
+	}
+  
+	getProjects(): void {
+		this.projects = this.projectsService.getProjects().slice().reverse();
+		let random = Math.floor(Math.random() * this.projects.length);
+		this.randomImage = this.projects[random].image;
+		this.randomLink = this.projects[random].link;
+		this.randomTitle = this.projects[random].title;
+	}
 }
