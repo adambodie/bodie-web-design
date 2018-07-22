85c0208af3fdefe2028db4a17153dbfaeb04089a
adambodie <adam.bodie@yahoo.com>
2018-07-21 22:16:06 -0700

    Updated howManyDaysAgo testing suite

--- src/howManyDaysAgo.spec.ts
+++ src/howManyDaysAgo.spec.ts
@@ -7,36 +7,74 @@ describe("How Many Days Ago", function() {
   });
   it("should return 2 years ago", function() {
     let date = new Date("July 4, 2018");
-    expect(howManyDaysAgo(2016, 0, 1, date)).toBe("2 years ago.");
+    let testDate = new Date("January 1, 2016");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("2 years ago.");
   });
   
   it("should return 1 year ago", function() {
     let date = new Date("July 4, 2018");
-    expect(howManyDaysAgo(2017, 0, 1, date)).toBe("1 year ago.");
+    let testDate = new Date("January 1, 2017");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("1 year ago.");
   });
+  
+  it("should return 12 months ago", function() {
+    let date = new Date("December 31, 2017");
+    let testDate = new Date("January 1, 2017");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("12 months ago.");
+  });  
   
   it("should return 5 months ago", function() {
-    let date = new Date("July 4, 2018");
-    expect(howManyDaysAgo(2018, 0, 1, date)).toBe("5 months ago.");
+    let date = new Date("June 1, 2018");
+    let testDate = new Date("January 1, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("5 months ago.");
   });
-  
-  it("should return 3 weeks ago", function() {
-    let date = new Date("July 1, 2018");
-    expect(howManyDaysAgo(2018, 5, 7, date)).toBe("3 weeks ago.");
+ 
+   it("should return 1 month ago", function() {
+    let date = new Date("January 31, 2018");
+    let testDate = new Date("January 1, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("1 month ago.");
   });
 
-  it("should return 3 days ago", function() {
-    let date = new Date("July 8, 2018");
-    expect(howManyDaysAgo(2018, 6, 4, date)).toBe("3 days ago.");
+   it("should return 4 weeks ago", function() {
+    let date = new Date("January 30, 2018");
+    let testDate = new Date("January 1, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("4 weeks ago.");
+  });
+  
+  it("should return 2 weeks ago", function() {
+    let date = new Date("July 21, 2018");
+    let testDate = new Date("July 7, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("2 weeks ago.");
+  });
+
+  it("should return 1 week ago", function() {
+    let date = new Date("July 14, 2018");
+    let testDate = new Date("July 7, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("1 week ago.");
+  });
+
+  it("should return 6 days ago", function() {
+    let date = new Date("July 11, 2018");
+    let testDate = new Date("July 5, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("6 days ago.");
   });
     it("should return 2 days ago", function() {
     let date = new Date("July 8, 2018");
-    expect(howManyDaysAgo(2018, 6, 5, date)).toBe("2 days ago.");
+    let testDate = new Date("July 6, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("2 days ago.");
   });
-	// this test fails
+
    it("should return 1 day ago", function() {
     let date = new Date("July 8, 2018");
-    expect(howManyDaysAgo(2018, 6, 6, date)).toBe("1 day ago.");
+    let testDate = new Date("July 7, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("Yesterday.");
   });
+  
+   it("should return Today", function() {
+    let date = new Date("July 8, 2018");
+    let testDate = new Date("July 8, 2018");
+    expect(howManyDaysAgo(testDate.getFullYear(), testDate.getMonth(), testDate.getDate(), date)).toBe("Today.");
+  }); 
+
    
 });
--- src/howManyDaysAgo.ts
+++ src/howManyDaysAgo.ts
@@ -1,43 +1,48 @@
 export const howManyDaysAgo = (years, months, days, current) => {
-  let update = new Date();
-  update.setFullYear(Number.parseInt(years), Number.parseInt(months), Number.parseInt(days));
-  let daysAgo = (current.getTime() - update.getTime())/86400000;
-  if (daysAgo >= -1) {
-      if (daysAgo > 365) {
-      let yearsAgo = daysAgo / 365;
-      let ago = (daysAgo <= 729) ? " year ago." : " years ago.";
-        return Math.floor(yearsAgo) + ago;
-      }
+	let update = new Date();
+	update.setFullYear(Number.parseInt(years), Number.parseInt(months), Number.parseInt(days));
+	update.setHours(0);
+	update.setMinutes(0);
+	update.setSeconds(0);
+	current.setHours(0);
+	current.setMinutes(0);
+	current.setSeconds(0);
+	let daysAgo = Math.round((current.getTime() - update.getTime())/86400000);
+	  
+	if (daysAgo >= -1) {
+		if (daysAgo > 365) {
+			let yearsAgo = daysAgo / 365;
+			let ago = (daysAgo <= 729) ? " year ago." : " years ago.";
+			return Math.floor(yearsAgo) + ago;
+		}
 
-      if (daysAgo > 31) {
-      let monthsAgo = daysAgo / 31;
-      let ago = (daysAgo <= 60) ? " month ago." : " months ago.";
-        return Math.floor(monthsAgo) + ago;
-      }
+		if (daysAgo >= 30) {
+			let monthsAgo = Math.round(daysAgo / 30);
+			let ago = (daysAgo <= 60) ? " month ago." : " months ago.";
+			return Math.round(monthsAgo) + ago;
+		}
 
-      if (daysAgo > 7) {
-      let weeksAgo = daysAgo / 7;
-      let ago = (daysAgo <= 13) ? " week ago." : " weeks ago.";
-        return Math.round(weeksAgo) + ago;
-      }
-      if (daysAgo >= 2) {
-        let ago = " days ago.";
-        return Math.round(daysAgo) + ago;
-      }
-      if (daysAgo <=1 && daysAgo > 0) {
-          let ago = " day ago.";
-          return Math.round(daysAgo) + ago;
-      }
-      if (Math.floor(daysAgo) <= -1) {
-        let ago = " days ago.";
-        return "Today";
-      }
-  } else {
-      return undefined;
-  };
+		if (daysAgo >= 7) {
+			let weeksAgo = Math.round(daysAgo / 7);
+			let ago = (daysAgo < 14) ? " week ago." : " weeks ago.";
+			return weeksAgo + ago;
+		}
+		
+		if (daysAgo >= 2) {
+			let ago = " days ago.";
+			return daysAgo + ago;
+		}
+		
+		if (daysAgo == 1) {
+			return "Yesterday.";
+		}
+		
+		if (daysAgo == 0) {
+			return "Today.";
+		}
+		
+	  } else {
+		  return undefined;
+	  };
 }
-export const setDate = (years, months, days) => {
-  let update = new Date();
-  update.setFullYear(Number.parseInt(years), Number.parseInt(months), Number.parseInt(days));
-  return update.toDateString();
-}
+
