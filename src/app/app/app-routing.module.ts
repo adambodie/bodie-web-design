dd7aaf45711a98cc7e0a868d2fe23011f1f5a9e9
Adam Bodie <adam.bodie@yahoo.com>
2019-03-30 20:29:09 -0700

    Fixed broken links

--- src/app/app/app-routing.module.ts
+++ src/app/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent} from './home/home.component';
-import { AboutComponent }      from './about/about.component';
-import { PicturesComponent } from './pictures/pictures.component';
+import { HomeComponent} from '../home/home.component';
+import { AboutComponent }      from '../about/about.component';
+import { PicturesComponent } from '../pictures/pictures.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -10,7 +10,7 @@ import { ProjectsComponent } from '../projects/projects.component';
 import { UpdateComponent } from '../update/update.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
-import { AppRoutingModule } from '../app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AboutComponent } from '../about/about.component';
 import { HomeComponent } from '../home/home.component';
 import { PicturesComponent } from '../pictures/pictures.component';
