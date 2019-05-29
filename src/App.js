d6be5addb9d9c79fbb6bf1f9a42821c61b4f613b
adambodie <adam.bodie@yahoo.com>
2019-05-28 17:40:04 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.scss';
+
+const App = () => {
+	return (
+		<div className="App container">
+			<h1>Header</h1>
+		</div>
+	);
+}
+
+export default App;
--- src/app/projects/howManyDaysAgo/language.ts
+++
@@ -1,3 +0,0 @@
-export class Language {
-    name: string;
-}
