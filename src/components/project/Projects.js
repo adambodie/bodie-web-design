36866a82ac05085d66e049922947a0b255d7acba
adambodie <adam.bodie@yahoo.com>
2019-06-23 20:03:29 -0700

    Added sort and filter methods to Projects

--- src/components/project/Projects.js
+++ src/components/project/Projects.js
@@ -8,9 +8,19 @@ export default class Projects extends Component {
 	constructor(){
 		super();
 		this.state = {
-			count: 0
+			count: 10,
+			updateCount: 0,
+			createCount: 0,
+			titleCount: 0,
+			titleOrder: 'A-Z',
+			createOrder: 'Oldest',
+			updateOrder: 'Oldest'
 		}
 		this.filterList = this.filterList.bind(this);
+		this.resetFilter = this.resetFilter.bind(this);
+		this.sortByTitle = this.sortByTitle.bind(this);
+		this.sortByCreatedDate = this.sortByCreatedDate.bind(this);
+		this.sortByUpdatedDate = this.sortByUpdatedDate.bind(this);
 	}
 	
 	filterList(event){
@@ -28,6 +38,77 @@ export default class Projects extends Component {
 		}
 		this.setState({count: newCount});
 	}
+	resetFilter() {
+		for (let item of store.getState().projects) {
+			item.visible = true;
+			}
+		this.setState({count: 10});
+	}
+	compareTitle(a,b) {
+		if (a.title > b.title) {
+			return -1;
+		}
+		if (a.title < b.title) {
+			return 1;
+		}
+		return 0;
+	}
+
+	compareUpdated(a,b) {
+		if (a.updated > b.updated) {
+			return -1;
+		}
+		if (a.updated < b.updated) {
+			return 1;
+		}
+		return 0;
+	}
+    compareCreated(a,b) {
+		if (a.created > b.created) {
+			return -1;
+		}
+		if (a.created < b.created) {
+			return 1;
+		}
+		return 0;
+	}
+	sortByUpdatedDate() {
+		if (this.state.updateCount % 2 === 0) {
+			store.getState().projects.sort(this.compareUpdated).reverse();
+			this.setState({updateOrder: 'Newest'});
+		} else {
+			store.getState().projects.sort(this.compareUpdated);
+			this.setState({updateOrder: 'Oldest'});
+		}
+		let newUpdateCount = this.state.updateCount;
+		newUpdateCount++;
+		this.setState({updateCount: newUpdateCount});
+	}
+	sortByCreatedDate() {
+		if (this.state.createCount % 2 === 0) {
+			store.getState().projects.sort(this.compareCreated).reverse();
+			this.setState({updateOrder: 'Newest'});
+		} else {
+			store.getState().projects.sort(this.compareCreated);
+			this.setState({updateOrder: 'Oldest'});
+		}
+		let newCreateCount = this.state.createCount;
+		newCreateCount++;
+		this.setState({createCount: newCreateCount});
+	}
+	sortByTitle() {
+		if (this.state.titleCount % 2 === 0) {
+			store.getState().projects.sort(this.compareTitle).reverse();
+			this.setState({titleOrder: "Z-A"});
+		} else {
+			store.getState().projects.sort(this.compareTitle);
+			this.setState({titleOrder: "A-Z"});
+		}
+		let newTitleCount = this.state.titleCount;
+		newTitleCount++;
+		this.setState({titleCount: newTitleCount});
+
+	}	
 	render(){
 		let { uniqueLanguages, projects } = store.getState();
 		return (
@@ -45,7 +126,7 @@ export default class Projects extends Component {
 							</select>
 							</div>
 							<div className="col-md-4">
-								<button className="btn" onClick={() => console.log("Testing")}>Reset</button>
+								<button className="btn" onClick={() => this.resetFilter()}>Reset</button>
 							</div>
 							<div className="col-md-3">
 								<h5>Results {this.state.count}</h5>
@@ -56,13 +137,13 @@ export default class Projects extends Component {
 						<h3>Sort Projects</h3>
 						<div className="row">
 							<div className="col-md-4">
-								<a className="btn" href="/">Title</a>
+								<button className="btn" onClick={()=> this.sortByTitle()}>Title {this.state.titleOrder}</button>
 							</div>
 							<div className="col-md-4">
-								<a className="btn" href="/">Created Order</a>
+								<button className="btn" onClick={()=> this.sortByCreatedDate()}>Created {this.state.createOrder}</button>
 							</div>
 							<div className="col-md-4">
-								<a className="btn" href="/">Updated Order</a>
+								<button className="btn" onClick={()=> this.sortByUpdatedDate()}>Update {this.state.updateOrder}</button>
 							</div>
 						</div>
 					</div>
