64f731194bc338accc9d6bffc1ee036d5bf16c85
adambodie <adam.bodie@yahoo.com>
2019-07-18 20:20:28 -0700

    cleaned up action variables, added new text to About component

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,38 +1,39 @@
-import { COUNTER, RESET, SORT_BY_TITLE, SORT_BY_CREATED, SORT_BY_UPDATED } from '../constants'
+import * as types from '../constants';
 
 export function counter(text) {
 	return {
-		type: COUNTER,
+		type: types.COUNTER,
 		text
 	}
 }
 
 export function reset(text) {
 	return {
-		type: RESET,
+		type: types.RESET,
 		text
 	}
 }
 
 export function sortByTitle(text) {
 	return {
-		type: SORT_BY_TITLE,
+		type: types.SORT_BY_TITLE,
 		text
 	}
 }
 
 export function sortByCreated(text) {
 	return {
-		type: SORT_BY_CREATED,
+		type: types.SORT_BY_CREATED,
 		text
 	}
 }
 
 export function sortByUpdated(text) {
 	return {
-		type: SORT_BY_UPDATED,
+		type: types.SORT_BY_UPDATED,
 		text
 	}
 }
+
 
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
 
-
-const store = createStore(
-	rootReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
+
+
