2a014b2c3e283cd08d3e3c85cbaaff1190132b43
Adam Bodie <adam.bodie@yahoo.com>
2019-09-28 21:26:16 -0600

    Added updated project info

--- src/store/constants/updates.js
+++ src/store/constants/updates.js
@@ -1,4 +1,7 @@
 export const UPDATES = [
+	{ "title": "Adam's Adventures", "description": "Launched version 2.0, backed with GatsbyJS and compiling data from GraphQL. September 5, 2019" },
+	{ "title": "NFL Passer Rating", "description": "Added Firebase to retrieve team data via Realtime Database.  August 4, 2019" },
+	{ "title": "Reno Nice 'n' Cool", "description": "Cleaned up Vue code, added Roulette component.  May 19, 2019" },
 	{ "title": "Dreidel Duel", "description": "Upgraded UI.  April 29, 2019" },
 	{ "title": "Adam's Black Ball", "description": "Upgraded UI, added Keyboard Component.  April 28, 2019" },
 	{ "title": "Adam's Adventures", "description": "Upgraded UI, added Jest/Redux.  March 19, 2019" },
